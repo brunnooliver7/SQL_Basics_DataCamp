5a153bff6ce9f8e882480bfaf4072a1bbe4f945c
Bruno Oliveira <brunno.oliver7@gmail.com>
2022-04-22 20:33:29 -0300

    Part 1 - Selecting columns

---
+++ 1-selecting-columns.sql
@@ -0,0 +1,68 @@
+-- Onboarding | Errors
+
+SELECT 'DataCamp <3 SQL'
+AS result;
+
+-- Onboarding | Multi-step Exercies
+
+SELECT 'SQL'
+AS result;
+
+SELECT 'SQL is'
+AS result;
+
+SELECT 'SQL is cool'
+AS result;
+
+-- SELECTing single columns
+
+SELECT title 
+FROM films;
+
+SELECT release_year 
+FROM films;
+
+SELECT name 
+FROM people;
+
+-- SELECTing multiple columns
+
+SELECT title 
+FROM films;
+
+SELECT title, release_year
+FROM films;
+
+SELECT title, release_year, country
+FROM films;
+
+SELECT *
+FROM films;
+
+-- SELECT DISTINCT
+
+SELECT DISTINCT country
+FROM films;
+
+SELECT DISTINCT certification
+FROM films;
+
+SELECT DISTINCT role
+FROM roles;
+
+-- Practice with COUNT
+
+SELECT count(*)
+FROM people;
+
+SELECT COUNT(birthdate)
+FROM people;
+
+SELECT COUNT(DISTINCT birthdate)
+FROM people;
+
+SELECT count(DISTINCT language)
+FROM films;
+
+SELECT count(DISTINCT country)
+FROM films;
